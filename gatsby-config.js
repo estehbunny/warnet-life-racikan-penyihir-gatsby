2a3e60815823422bb0f3579aac629a1b77d41ce3
estehbunny <estehbunny@gmail.com>
2022-06-28 23:37:03 +0700

    Updated manifest & added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,5 +4,32 @@ module.exports = {
     siteUrl: `https://estehbunny.github.io/warnet-life-racikan-penyihir-gatsby`
   },
   pathPrefix: '/reponame',
-  plugins: ["gatsby-plugin-sass"]
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Warnet Life - Racikan Penyihir`,
+        short_name: `Warnet Life - Racikan Penyihir`,
+        start_url: `/`,
+        background_color: `#4c3957`,
+        theme_color: `#50485b`,
+        display: `standalone`,
+        icon: `src/favicons/favicon.png`,
+        icons: [
+          {
+            src: `src/favicons/favicon-192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `src/favicons/favicon-512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ]
+      }
+    },
+    'gatsby-plugin-offline'
+  ]
 }
